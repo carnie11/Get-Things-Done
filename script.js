cdc9e5dd36450bf68260e44c152d34d532a6333d
carnie11 <carolin.modigh@gmail.com>
2022-11-19 10:23:40 +0100

    Cleanup

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ let tabell = document.getElementById("tabell");
 
 header.innerHTML="To do list";
 
-// class doneButton {                  ////////////CLASS FÖR DONE!-BUTTON
+// class doneButton {                           //CLASS FÖR DONE!-BUTTON
 //     constructor() {
 //         this.value= "Done!";
 //         this.addEventListener("click", console.log("Klick på Done!"))  {
@@ -36,7 +36,7 @@ header.innerHTML="To do list";
 //------------------------------------//
 
 
-for (let i=0; i < taskList.length; i++) {       //////LOOP FÖR ATT FÅ ARRAYEN IN PÅ TABELLEN + LÄGGA TILL EN KNAPP
+for (let i=0; i < taskList.length; i++) {       //LOOP FÖR ATT FÅ ARRAYEN IN PÅ TABELLEN + LÄGGA TILL EN KNAPP
     let row = tabell.insertRow(-1);
     let firstNameCell = row.insertCell(-1);
     firstNameCell.appendChild(document.createTextNode(taskList[i]));
@@ -45,16 +45,16 @@ for (let i=0; i < taskList.length; i++) {       //////LOOP FÖR ATT FÅ ARRAYEN 
 }
 
 
-function onAddATask(e) {            ////FUNKTION FÖR DET SOM SKA HÄNDA NÄR MAN LÄGGER TILL TASKS PÅ LISTAN        
+function onAddATask(e) {                         //FUNKTION FÖR DET SOM SKA HÄNDA NÄR MAN LÄGGER TILL TASKS PÅ LISTAN        
     e.preventDefault();             
     let nyttTask = form.value;
-    let nyRad = document.createElement("tr");   ////SKAPAR NY RAD OCH LÄGGER IN INPUTET + PUSHAR DET TILL TASKLIST-ARRAYEN
+    let nyRad = document.createElement("tr");   //SKAPAR NY RAD OCH LÄGGER IN INPUTET + 
     tHead.appendChild(nyRad);
     nyRad.innerHTML=nyttTask;
-    taskList.push(nyttTask);
+    taskList.push(nyttTask);                    //PUSHAR INPUT TILL TASKLIST-ARRAYEN
     console.log(taskList);
 
-    let nyCell = document.createElement("td");  ////SKAPAR EN NY DONE!-BUTTON BREDVID DET TILLAGDA INPUTET
+    let nyCell = document.createElement("td");  //SKAPAR EN NY DONE!-BUTTON BREDVID DET TILLAGDA INPUTET
     nyRad.appendChild(nyCell);
     nyCell.innerHTML= "<button>Done!</button>";
 }
