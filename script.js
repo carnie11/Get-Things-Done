cba8e35bf16d7ebed8def32682691563d0d6ea6d
carnie11 <carolin.modigh@gmail.com>
2022-11-19 10:00:32 +0100

    Cleanup

--- script.js
+++ script.js
@@ -5,9 +5,7 @@
 let header = document.getElementById("header");
 let addATask = document.getElementById("addATask");
 let tHead = document.getElementById("tHead");
-let taskList = [{Name: "Äta", Btn: "<button></button>"}, 
-                {Name: "Sova", Btn: "<button>Done!</button>"}, 
-                {Name: "Städa", Btn: "<button>Done!</button>"}, ]
+let taskList = ["Äta", "Sova", "Städa", ]
 //let buttonList = ["<button>Done!</button>", "<button></button>","<button></button>", ]
 
 //let mitten = document.getElementById("mitten");
@@ -38,22 +36,23 @@ header.innerHTML="To do list";
 //------------------------------------//
 
 
-for (let i=0; i < taskList.length; i++) {       //////LOOP FÖR ATT FÅ ARRAYEN IN PÅ TABELLEN
+for (let i=0; i < taskList.length; i++) {       //////LOOP FÖR ATT FÅ ARRAYEN IN PÅ TABELLEN + LÄGGA TILL EN KNAPP
     let row = tabell.insertRow(-1);
     let firstNameCell = row.insertCell(-1);
-    firstNameCell.appendChild(document.createTextNode(taskList[i].Name));
-
-    // let lastNameCell = row.insertCell(-1);
-    // lastNameCell.appendChild(document.createElement(taskList[i].Btn));
+    firstNameCell.appendChild(document.createTextNode(taskList[i]));
+    firstNameCell.insertAdjacentHTML("afterend",  "<button>Done!</button>");
+    
 }
 
 
 function onAddATask(e) {            ////FUNKTION FÖR DET SOM SKA HÄNDA NÄR MAN LÄGGER TILL TASKS PÅ LISTAN        
     e.preventDefault();             
     let nyttTask = form.value;
-    let nyRad = document.createElement("tr");   ////SKAPAR NY RAD OCH LÄGGER IN INPUTET
+    let nyRad = document.createElement("tr");   ////SKAPAR NY RAD OCH LÄGGER IN INPUTET + PUSHAR DET TILL TASKLIST-ARRAYEN
     tHead.appendChild(nyRad);
     nyRad.innerHTML=nyttTask;
+    taskList.push(nyttTask);
+    console.log(taskList);
 
     let nyCell = document.createElement("td");  ////SKAPAR EN NY DONE!-BUTTON BREDVID DET TILLAGDA INPUTET
     nyRad.appendChild(nyCell);
